a742c18282de06c578c18e9ad9476f2e535e585f
Mark Pirogov <m4rkpirogov@gmail.com>
2023-02-02 18:44:43 +0300

    feat: add main page menu routing functionality and README.md project discription

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,6 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import {
   DirectoryItemContainer,
   BackgroundImage,
@@ -5,10 +8,16 @@ import {
 } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = useCallback(
+    () => navigate(route),
+    [navigate, route]
+  );
 
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
