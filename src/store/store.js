8f91342d486ffdc272973eb462b3d92ea9867528
Mark Pirogov <m4rkpirogov@gmail.com>
2023-02-15 22:06:51 +0300

    feat: add redux-persist and configured the logger and redux devtools
    Signed-off-by: Mark Pirogov <m4rkpirogov@gmail.com>

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,28 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const middlewares = [logger];
+const presistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+}
 
-const composeEnhancers = compose(applyMiddleware(...middlewares));
+const persistedReducer = persistReducer(presistConfig, rootReducer)
 
-export const store = createStore(rootReducer, undefined, composeEnhancers);
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composeEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persistor = persistStore(store)
